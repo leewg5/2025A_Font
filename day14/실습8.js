9992920e97ac70c13cbeed8472fb43b0811da8fe
리우지5 <tosknm5@gmail.com>
2025-06-17 18:19:30 +0900

    실습10번 작업중

--- day14/실습8.js
+++ day14/실습8.js
@@ -30,6 +30,42 @@ const members = [
 ];
 */
 
+const members = [
+  { id: 'user1', password: 'pass1', name: '사용자1' },
+  { id: 'user2', password: 'pass2', name: '사용자2' },
+];
+
+let id = prompt('아이디:');
+let pwd = prompt('비밀번호:');
+let name = prompt('이름:');
+let member = { id : id , pwd : pwd , name : name }
+// [] : 배열 // {} : 객체
+
+//let member = {}
+//member.id = id;
+//member.pwd = pwd;
+//member.name = name; // vs  let member = { id : id , pwd : pwd , name : name }
+//let member = []
+//member.push(id)
+
+/*
+let idcheck = false;
+for(let index = 0; index <= members.length -1 ; index++){
+  let member = members[index];  // index번째의 member 객체 조회(꺼내기)
+  if(member.id == id){    
+    idcheck = true; //중복체크
+    break; // 반복문 종료
+  //}else{  //같지 않으면
+    members.push(member)
+  } // 아직 모두 조회된 상태가 아니므로 for문 안에서 배열에 저장 하지 않는다.
+} // for end
+if(idcheck == false ){
+  members.push(member);
+  console.log('등록 성공');
+}else{
+  console.log('등록 실패');
+}
+*/
 
 
 /*
@@ -41,18 +77,67 @@ const scores = [
   { name: 'C', math: 76, science: 78 }
 ];
 */
+/* 
+const scores = [
+  { name: 'A', math: 80, science: 92 },
+  { name: 'B', math: 95, science: 88 },
+  { name: 'C', math: 76, science: 78 }
+];
 
+let 학생객체1 = scores[0]
+let 학생객체2 = scores[1]
+let 학생객체3 = scores[2]
+let 학생1수학점수 = 학생객체1.math;
+let 학생2수학점수 = 학생객체2.math;
+let 학생3수학점수 = 학생객체3.math;
+let 합계 = 학생1수학점수 + 학생2수학점수 + 학생3수학점수 ;
+let 인원수 = scores.length // 3
+let 평균 = 합계 / 인원수 ;
+*/
+/*
+let 합계 = 0;
+for(let index=0; index <= scores.length -1 ; index++){    // 0부터 마지막 인덱스까지 1씩증가
+  let 학생객체 = scores[index]; // index번째 학생객체 호출
+  합계 += 학생객체.math; // index번째 학생객체의 수학점수 속성값 호출
+} //for end
+console.log(합계 / scores,length);   // 합계/3(배열길이=객체개수=인원수)
+*/
 
 
 /*
 문제 4: 특정 조건을 만족하는 객체 찾기
-products 배열에서 id가 3인 상품 객체를 찾아, 해당 객체 전체를 콘솔에 출력하시오. 일치하는 객체가 없으면 "상품을 찾을 수 없습니다."를 출력합니다.
+products 배열에서 id가 3인 상품 객체를 찾아, 
+해당 객체 전체를 콘솔에 출력하시오. 
+일치하는 객체가 없으면 "상품을 찾을 수 없습니다."를 출력합니다.
 const products = [
   { id: 1, name: '사과' },
   { id: 2, name: '바나나' },
   { id: 3, name: '포도' },
   { id: 4, name: '딸기' }
 ];
+*/
+/*
+const products = [
+  { id: 1, name: '사과' },
+  { id: 2, name: '바나나' },
+  { id: 3, name: '포도' },
+  { id: 4, name: '딸기' }
+];
+
+let idcheck = false; // id가 3인 찾았다(true)/못찾았다(false)
+for(let index= 0; index <= products.length -1 ; index++){
+  let 과일객체 = products[index]; 
+  if(과일객체.id == 3 ){
+    idcheck = true;
+    console.log(과일객체)
+    //let idcheck = true; //찾았다. 목표 이뤘다.
+    break;
+  }
+  // * 모두 조회(for문)를 하고나서 찾았다/못찾았다. 판단!!
+} //for end
+if(idcheck == false){
+  console.log("상품을 찾을 수 없습니다.");
+}
 */
 
 
@@ -67,6 +152,28 @@ const users = [
 ];
 */
 
+/*
+const users = [
+  { id: 1, name: '유저1', isActive: true },
+  { id: 2, name: '유저2', isActive: false },
+  { id: 3, name: '유저3', isActive: true },
+  { id: 4, name: '유저4', isActive: false }
+];
+const activeUser = [] //새로운 배열인 빈배열 선언
+for(let index=0; index <= users.length-1; index++){
+  if(users[index].isActive == true){activeUser.push(users[index]);}
+}
+console.log(activeUser);
+*/
+
+/*
+const activeUsers=[] // 새로운 배열인 빈배열 선언
+if(user[0].isActive == true) activeUsers.push(users[0])
+if(user[1].isActive == true) activeUsers.push(users[1])
+if(user[2].isActive == true) activeUsers.push(users[2])
+if(user[3].isActive == true) activeUsers.push(users[3])
+*/
+
 
 /*
 문제 6: 객체 배열 데이터 변환하기
@@ -77,8 +184,18 @@ const movies = [
   { title: '매트릭스', director: '워쇼스키 자매' }
 ];
 */
+/*
+const movies = [
+  { title: '인셉션', director: '크리스토퍼 놀란' },
+  { title: '기생충', director: '봉준호' },
+  { title: '매트릭스', director: '워쇼스키 자매' }
+];
 
-
+const movieTitles = []
+movieTitles.push(movies[0].title);
+movieTitles.push(movies[1].title);
+movieTitles.push(movies[2].title);
+*/
 
 /*
 문제 7: 데이터 그룹화하기
@@ -96,6 +213,35 @@ const team = [
 // }
 */
 
+/*
+const team = [
+  { name: '철수', department: '개발팀' },
+  { name: '영희', department: '기획팀' },
+  { name: '민수', department: '개발팀' },
+  { name: '지혜', department: '기획팀' }
+];
+let result = {'개발팀':[], '기획팀':[]}; //빈 배열(부서) 2개를 갖는 객체 생성
+*/
+/*
+if(team[0].department == "개발팀"){
+  result.개발팀.push(team[0].name);
+}else if( team[0].department == "기획팀"){
+  result.기획팀.push(tram[0].name);
+}
+*/
+/*
+for(let index=0; index <= team.length-1; index++){
+  let t = team[index]; // index번째 객체 호출
+  if(t.department == '개발팀'){ //index번째 객체의 부서가 '개발팀'
+    result.개발팀.push(t.name); // 결과객체내 개발팀 배열에 index번째 객체의 이름 저장 
+  }else if(t.department == '기획팀'){
+    result.기획팀.push(t.name);
+  }
+} // for end
+console.log(result);
+*/
+
+
 
 /*
 문제 8: 장바구니 총액 계산하기
@@ -110,7 +256,12 @@ const productsInfo = [
   { id: 3, price: 2500 }
 ];
 */
-
+const cart = [{ id: 1, quantity: 2 },{ id: 3, quantity: 1 }];
+const productsInfo = [
+  { id: 1, price: 1000 },
+  { id: 2, price: 5000 }, // 장바구니에 없는 상품
+  { id: 3, price: 2500 }
+];
 
 
 
@@ -164,8 +315,3 @@ const webtoons = [
   6. 인증키 설정 후 'API 목록' 에서  [Open Api 호출] 합니다.
   7. **실행 결과(JSON)**를 전체 복사합니다.  
 */
-
-
-
-
-
